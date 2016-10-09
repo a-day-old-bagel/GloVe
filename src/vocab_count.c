a6cba718ab1872109df4e57fe4bfe4601b6ab296
Galen Cochrane <cochgale@isu.edu>
2016-10-08 22:02:55 -0600

    removed commented-out mains

--- src/vocab_count.c
+++ src/vocab_count.c
@@ -176,29 +176,6 @@ static int get_counts() {
     return 0;
 }
 
-/*int main(int argc, char **argv) {
-    int i;
-    if (argc == 1) {
-        printf("Simple tool to extract unigram counts\n");
-        printf("Author: Jeffrey Pennington (jpennin@stanford.edu)\n\n");
-        printf("Usage options:\n");
-        printf("\t-verbose <int>\n");
-        printf("\t\tSet verbosity: 0, 1, or 2 (default)\n");
-        printf("\t-max-vocab <int>\n");
-        printf("\t\tUpper bound on vocabulary size, i.e. keep the <int> most frequent words. The minimum frequency words are randomly sampled so as to obtain an even distribution over the alphabet.\n");
-        printf("\t-min-count <int>\n");
-        printf("\t\tLower limit such that words which occur fewer than <int> times are discarded.\n");
-        printf("\nExample usage:\n");
-        printf("./vocab_count -verbose 2 -max-vocab 100000 -min-count 10 < corpus.txt > vocab.txt\n");
-        return 0;
-    }
-    
-    if ((i = find_arg((char *)"-verbose", argc, argv)) > 0) verbose = atoi(argv[i + 1]);
-    if ((i = find_arg((char *)"-max-vocab", argc, argv)) > 0) max_vocab = atoll(argv[i + 1]);
-    if ((i = find_arg((char *)"-min-count", argc, argv)) > 0) min_count = atoll(argv[i + 1]);
-    return get_counts();
-}*/
-
 static const VocabCountArgs DEFAULT_VOCABCOUNT_ARGS = {
         .verbose = 0, .maxVocab = -1, .minCount = 1
 };
